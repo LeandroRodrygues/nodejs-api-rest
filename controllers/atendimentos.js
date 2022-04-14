eac177a690f00825f72bf0fcaea32046adbfb434
Leandro <leogrcel@gmail.com>
2022-04-13 21:12:51 -0300

    Adicionando body-parser.json()

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,7 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está realizando um GET'));
-    app.post('/atendimentos', (req, res) => res.send('Você está realizando um POST'));
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send('Você está realizando um POST');
+    });
 }
