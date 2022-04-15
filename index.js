d11429cdc4283b4d6571d6a47a4255f04918706a
Leandro <leogrcel@gmail.com>
2022-04-15 14:31:13 -0300

    Criando Tabela atendimentos

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = customExpress();
 
 const conexao = require('./infraestrutura/conexao');
 
+const Tabelas = require('./infraestrutura/tabelas');
+
 conexao.connect((erro) => {
     if (erro){
         console.log(erro);
     }else{
-        console.log("Conectado com Sucesso!");        
+        console.log("Conectado com Sucesso!");
+        Tabelas.init(conexao);
         app.listen(3000, () => console.log('Servidor Rodando na porta 3000'));
     }
 });
