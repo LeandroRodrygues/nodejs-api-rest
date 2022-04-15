ea4c7fe560da732b3c5c64ee29699e4cac11ba39
Leandro <leogrcel@gmail.com>
2022-04-15 13:06:25 -0300

    Corrigindo conexão do mysql

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ const customExpress = require('./config/customExpress');
 
 const app = customExpress();
 
-app.listen(3000, () => console.log('Servidor Rodando na porta 3000'));
+const conexao = require('./infraestrutura/conexao');
+
+conexao.connect((erro) => {
+    if (erro){
+        console.log(erro);
+    }else{
+        console.log("Conectado com Sucesso!");        
+        app.listen(3000, () => console.log('Servidor Rodando na porta 3000'));
+    }
+});
+
+
 
