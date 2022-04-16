29619f7723a9e9c6fd314f1d048e76c5bfe45bdd
Leandro <leogrcel@gmail.com>
2022-04-15 21:07:33 -0300

    Adicionando biblioteca moment

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,7 @@
 const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está realizando um GET'));
+    app.get('/atendimentos', (_req, res) => res.send('Você está realizando um GET'));
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
         Atendimento.adiciona(atendimento);
